0184185a8f3b3a7c1a59d0b911f9b997cb31bcea
Liam-YorkRobertson <105305808+Liam-YorkRobertson@users.noreply.github.com>
2023-04-09 14:57:08 +0200

    finished the unit converter, added distance and mass conversions

--- c/unit_converter/unit_converter.c
+++ c/unit_converter/unit_converter.c
@@ -17,6 +17,20 @@ int main() {
     float calcCelsToKel;
     float kelToCels;
     float calcKelToCels;
+    int massConversion;
+    float kiloToPound;
+    float calcKiloToPound;
+    float poundToKilo;
+    float calcPoundToKilo;
+    int lengthConversion;
+    float metreToFoot;
+    float calcMetreToFoot;
+    float footToMetre;
+    float calcFootToMetre;
+    float kiloToMile;
+    float calcKiloToMile;
+    float mileToKilo;
+    float calcMileToKilo;
 
     printf("Welcome to my unit conversion programme!\n");
     printf("These are the supported types of conversion:\n");
@@ -66,11 +80,67 @@ int main() {
     }
     else if (conversionType == 'M')
     {
+        printf("You have chosen to convert mass units\n");
+        printf("Which of the following conversions would you like to perform:\n");
+        printf("kilogram to pound (1)\n");
+        printf("pound to kilogram (2)\n");
+        printf("Enter your choice here: ");
+        scanf("%d", &massConversion);
 
+        if (massConversion == 1)
+        {
+            printf("Enter the kilogram value you would like to convert to pound: ");
+            scanf("%f", &kiloToPound);
+            calcKiloToPound = (kiloToPound * 2.20462);
+            printf("That is %.2flb", calcKiloToPound);
+        }
+        else if (massConversion == 2)
+        {
+            printf("Enter the pound value that you would like to convert to kilogram: ");
+            scanf("%f", &poundToKilo);
+            calcPoundToKilo = (poundToKilo / 2.20462);
+            printf("That is %.2fkg", calcPoundToKilo);
+        }
     }
     else if (conversionType = 'L')
     {
+        printf("You have chosen to convert length units\n");
+        printf("Which of the following conversions would you like to perform:\n");
+        printf("metre to foot (1)\n");
+        printf("foot to metre (2)\n");
+        printf("kilometre to mile (3)\n");
+        printf("mile to kilometre (4)\n");
+        printf("Enter your choice here: ");
+        scanf("%d", &lengthConversion);
 
+        if (lengthConversion == 1)
+        {
+            printf("Enter the metre value that you would like to convert to foot: ");
+            scanf("%f", &metreToFoot);
+            calcMetreToFoot = (metreToFoot * 3.28084);
+            printf("That is %.2fft", calcMetreToFoot);
+        }
+        else if (lengthConversion == 2)
+        {
+            printf("Enter the foot value that you would like to convert to metre: ");
+            scanf("%f", &footToMetre);
+            calcFootToMetre = (footToMetre / 3.28084);
+            printf("That is %.2fm", calcFootToMetre);
+        }
+        else if (lengthConversion == 3)
+        {
+            printf("Enter the kilometre value that you would like to convert to mile: ");
+            scanf("%f", &kiloToMile);
+            calcKiloToMile = (kiloToMile * 0.62137119);
+            printf("That is %.2fmi", calcKiloToMile);
+        }
+        else if (lengthConversion == 4)
+        {
+            printf("Enter the mile value that you would like to convert to kilogram: ");
+            scanf("%f", &mileToKilo);
+            calcMileToKilo = (mileToKilo / 0.62137119);
+            printf("Tha is %.2fkm", calcMileToKilo);
+        }
     }
     else
     {
